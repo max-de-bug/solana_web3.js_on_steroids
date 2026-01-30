c86af05b3cbcdb6cc01f5a717003640222b3e7b4
konor <maxum_yuzva@ukr.net>
2026-01-31 01:10:34 +0200

    refactor: achieve full architectural consistency and resource safety

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -1,2 +1,10 @@
+export { SteroidClient } from './client/SteroidClient.js';
+export { SteroidConnection } from './connection/SteroidConnection.js';
+export { SteroidTransaction } from './transaction/SteroidTransaction.js';
 export { SteroidWallet, WalletError } from './wallet/SteroidWallet.js';
 export * from './types/SteroidWalletTypes.js';
+import { SteroidClient } from './client/SteroidClient.js';
+/**
+ * Factory function for creating a new SteroidClient instance.
+ */
+export declare const createSteroidClient: (url: string, config?: any) => SteroidClient;
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
+export { SteroidClient, createSteroidClient } from './client/SteroidClient.js';
+export { SteroidConnection } from './connection/SteroidConnection.js';
+export { SteroidTransaction } from './transaction/SteroidTransaction.js';
 export { SteroidWallet, WalletError } from './wallet/SteroidWallet.js';
 export * from './types/SteroidWalletTypes.js';
