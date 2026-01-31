6334a73cf1178cb786fd62f9355d89c7a60f25bc
konor <maxum_yuzva@ukr.net>
2026-01-31 17:42:55 +0200

    build: finalize project structure for npm release

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,6 @@
-export { SteroidClient } from './client/SteroidClient.js';
+export { SteroidClient, createSteroidClient } from './client/SteroidClient.js';
 export { SteroidConnection } from './connection/SteroidConnection.js';
 export { SteroidTransaction } from './transaction/SteroidTransaction.js';
 export { SteroidWallet, WalletError } from './wallet/SteroidWallet.js';
 export * from './types/SteroidWalletTypes.js';
-import { SteroidClient } from './client/SteroidClient.js';
-/**
- * Factory function for creating a new SteroidClient instance.
- */
-export const createSteroidClient = (url, config) => new SteroidClient(url, config);
 //# sourceMappingURL=index.js.map
